728faa87636115af2c965dc41ad0c6e5c6f7f6e3
GiuliaScagliarini <giulia.scagliarini@hotmail.com>
2022-12-15 16:26:51 +0100

    last

--- pag_burger.py
+++ pag_burger.py
@@ -1,63 +1,119 @@
 import streamlit as st
-import plotly.figure_factory as ff
-from PIL import Image
 import pandas as pd
-import numpy as np
-import joblib
 from prophet.plot import plot_plotly
-from datetime import datetime
-import matplotlib.pyplot as plt
+import joblib
+from pandas.plotting import lag_plot
+from statsmodels.tsa.stattools import acf
+from prophet.diagnostics import cross_validation,performance_metrics
+from datetime import date
+ 
 
 def main():
-    st.button("Re-run")
-    # set up layout
-    st.title("Benvenuto nella sezione dei burger!")
-    st.markdown("Di seguito alcune informazioni sulle vendite dei burger")
+    pd.options.display.float_format = '{:,.1f}'.format
+    st.subheader('Tabella dati input - Aggregazione giornaliera')
+    df = pd.read_excel('dati.xlsx', parse_dates=True,dtype={"Quantita":int})
+    copia_df=df.copy()
+    df_burger=copia_df[copia_df['Tipologia']=='Burger']
+    df_burger=df_burger.drop('Tipologia',axis=1)
+    df_burger['Data'] = df_burger['Calendario']
+    df_burger = df_burger.drop('Calendario',axis=1)
+    quante = st.slider("Selezionare la dimensione del dataset",100,len(df_burger),250)
+    df_burger = df_burger[['Data','Quantita']]
+    #df_burger['Quantita']=round(df_burger['Quantita'])
+    df_burger['Data'] = pd.to_datetime(df_burger['Data']).dt.date
+    df_burger['Data']=pd.to_datetime(df_burger['Data'],format='%Y/%m/%d')
+    df_burger['Data']=df_burger['Data'].dt.strftime('%d/%m/%Y')
+    df_burger.columns=['Data','Quantità']
+    st.dataframe(df_burger.head(quante))
 
+    st.subheader('Tabella statistica riassuntiva')
+    st.dataframe(df_burger.describe().T)
+
+    st.subheader('Istogramma sui dati di vendita dei burger')
+    bins_scelti = st.slider(
+        'Selezionare il numero di bins',
+        40, 100, 60)
+    isto = df_burger['Quantità'].plot(kind='hist', bins=bins_scelti)
+    st.pyplot(isto.figure,clear_figure=True)
+
+    st.subheader('Lag plot originale sui burger')
+    st.write ("""lag plot----blablab bla....descrivete ogni plot cosa rappresenta, bla bla bla bla bla bla bla bla bla bla bla bla
+                bla bla bla bla bla bla bla bla bla bla bla bla
+                bla bla bla bla bla bla bla bla bla bla bla bla
+                bla bla bla bla bla bla bla bla bla bla bla bla
+                bla bla bla bla bla bla bla bla bla bla bla bla
+                """)
+    lag = int(st.text_input(f'Scegli il lag (dev\'essere un numero intero compreso tra 1 e {len(df_burger)-1}!)',value=1))
+    if (lag>len(df_burger)-1) or (lag < 1):
+        st.write(f'Inserire un numero di giorni non troppo elevato (tra 1 e 100)')
+    else:
+        grafico_lag = lag_plot(df_burger['Quantità'],lag)
+        st.pyplot(grafico_lag.figure,clear_figure=True)
+
+        autocorrelation_vet = acf(df_burger['Quantità'],nlags=lag)
+        autocorrelation = autocorrelation_vet[-1]
+        st.write(f'L\'autocorrelazione di questo lag plot è del {round(100*autocorrelation,2)}%')
     
-    data=pd.read_excel('dati.xlsx', 
-                     index_col = 'Calendario',
-                     parse_dates = True
-                     )
-    df_burger = data[data['Tipologia'] == 'Burger']
-    df_burger = df_burger.drop('Tipologia', axis = 1)
-    df_burger = df_burger[(df_burger.index >= '2021-05-25') & (df_burger.index <= '2022-11-05')]
 
-    st.markdown('Distribuzione dei dati usati')
-    fig, ax = plt.subplots()
-    ax.hist(df_burger['Quantita'], bins=100)
-    plt.xlabel('Quantità vendute di burger')
-    plt.ylabel('Frequenza')
-    st.pyplot(fig)
+    st.subheader('Dataset senza outliers')
+    df_burger_rid = df_burger[(df_burger['Quantità']<130) & (df_burger['Quantità']>15)]
+    df_burger_rid = df_burger_rid.reset_index()
+    df_burger_rid = df_burger_rid.drop('index',axis=1)
+    quante2 = st.slider("Selezionare la dimensione del dataset.",100,len(df_burger),250)
+    st.dataframe(df_burger_rid.head(quante2))
 
-    df_burger = df_burger.reset_index()
-    df_burger.columns = ['ds', 'y']
+    st.subheader('Tabella statistica riassuntiva senza outliers')
+    st.dataframe(df_burger_rid.describe().T)
 
+    st.subheader('Istogramma sui burger senza outliers')
+    bins_scelti2 = st.slider(
+        'Selezionare il numero bins',
+        40, 100, 60)
+    isto2 = df_burger_rid['Quantità'].plot(kind='hist', bins=bins_scelti2)
+    st.pyplot(isto2.figure,clear_figure=True)
 
+    st.subheader('Lag plot sui burger senza outliers')
+    lag2 = int(st.text_input(f'Scegli il lag (dev\'essere un numero intero compreso tra 1 e {len(df_burger_rid)-1}!)',value=1))
+    if (lag2>len(df_burger_rid)-1) or (lag2 < 1):
+        st.write(f'Devi inserire un numero compreso tra 1 e {len(df_burger_rid)-1}!')
+    else:
+        grafico_lag2 = lag_plot(df_burger_rid['Quantità'],lag2)
+        st.pyplot(grafico_lag2.figure,clear_figure=True)
+        
+        autocorrelation_vet2 = acf(df_burger_rid['Quantità'],nlags=lag2)
+        autocorrelation2 = autocorrelation_vet2[-1]
+        st.write(f'L\'autocorrelazione di questo lag plot è del {round(100*autocorrelation2,2)}%')
 
     model = joblib.load('model_burger.pkl')
 
-
-    st.subheader('Previsioni')
-    add_slider = st.slider('Scegli il periodo di previsione dei dati', 10, 150)
-    future = model.make_future_dataframe(int(add_slider), freq='D')
+    st.subheader('Componenti dei burger senza outliers')
+    quanto_trend = st.slider('Scegliere il numero di giorni per verificare i diversi trend',7,180,60)
+    future = model.make_future_dataframe(periods=quanto_trend)
     forecast = model.predict(future)
+    comp = model.plot_components(forecast)
+    st.pyplot(comp.figure,clear_figure=True)
 
-
+    st.subheader('Forecasting')
+    da_pred = st.slider('Scegliere il numero di gironi di Forecast',7,180,60)
+    future = model.make_future_dataframe(da_pred, freq='D')
+    forecast = model.predict(future)
     fig = plot_plotly(model, forecast)
-    fig.update_layout(yaxis_title = 'Quantità di burger venduti',
-                xaxis_title = 'Data',
-                title = 'Vendita Burger'
-                )
+    fig.update_layout(title="Previsione dei burger venduti",
+                    yaxis_title='Burger venduti',
+                    xaxis_title="Data",
+                    )
+    fig.add_vline(x=date.today(), line_width=3, line_dash="dash", line_color="red")
+    fig.update_layout(width=1600)
     st.plotly_chart(fig)
 
-
-
-    if st.button('Mostrare componenti dei trend'):
-        st.pyplot(model.plot_components(forecast))
-    else:
-        print('Niente')
-
+    df_cv_final=cross_validation(model,
+                            horizon="120 days",
+                            period='10 days',
+                            initial='350 days',
+                            )
+    df_performance=performance_metrics(df_cv_final)
+    mape = df_performance['mape'].mean()
+    st.write(f'L\'errore percentuale medio della previsione calcolato sugli ultimi 60 giorni è del {round(mape*100,2)}%')
 
 
 
